4d676c36a38099fd6df5977a1a6f0765d858ddb2
Hemant <hemantcodes@gmail.com>
2025-12-02 19:35:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -721,6 +721,12 @@ def main():
             """)
     
     st.markdown("---")
+    st.markdown("""
+<div style="text-align: center; font-size: 15px; color: #777;">
+    Developed by <strong>Hemant Kumar</strong> • 
+    <a href="https://www.linkedin.com/in/hemantkumar2430" target="_blank">LinkedIn</a>
+</div>
+""", unsafe_allow_html=True)
     st.markdown(
         '<div style="text-align: center; color: #666; padding: 1rem;">Made with Streamlit & Google Earth Engine</div>',
         unsafe_allow_html=True,
@@ -728,3 +734,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
