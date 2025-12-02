647b11aa2f1cb046b9b14352c09a3ade8f16f18f
Hemant <hemantcodes@gmail.com>
2025-12-02 19:25:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -713,20 +713,20 @@ def main():
             - Available from: 2013
             - Best for: Long-term studies
             """)
-    st.markdown("""
-<div style="text-align: center; font-size: 15px; color: #777; margin-top: -15px;">
-    Developed by <strong>Hemant Kumar</strong> • 
-    <a href="https://www.linkedin.com/in/hemantkumar2430" target="_blank">LinkedIn</a>
-</div>
-""", unsafe_allow_html=True)
     st.markdown("---")
     st.markdown(
         '<div style="text-align: center; color: #666; padding: 1rem;">Made with Streamlit & Google Earth Engine</div>',
         unsafe_allow_html=True,
     )
-
+ st.markdown("""
+<div style="text-align: center; font-size: 15px; color: #777; margin-top: -15px;">
+    Developed by <strong>Hemant Kumar</strong> • 
+    <a href="https://www.linkedin.com/in/hemantkumar2430" target="_blank">LinkedIn</a>
+</div>
+""", unsafe_allow_html=True)
 if __name__ == "__main__":
     main()
+
 
 
 
